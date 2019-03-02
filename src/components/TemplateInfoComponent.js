d51dc490636acb98e4dd3409ceef6f16c25c33f0
Yuriy Stetskyi <ystetskyy333@gmail.com>
2019-03-02 13:17:02 +0200

    Designed TemplateInfoComponent

--- src/components/TemplateInfoComponent.js
+++ src/components/TemplateInfoComponent.js
@@ -8,13 +8,132 @@ class TemplateInfoComponent extends React.Component {
         this.props.getTemplate(this.props.match.params.id);
     }
 
+    removeTemplate = () => {
+        
+    };
+
     renderTemplate(){
         if(!this.props.template){
-            return <div>Loading...</div>
+            return <div>Loading...</div>;
         }
 
+        var filter = this.props.template.dataSourceFilter;
+
         return (
-            <div>{JSON.stringify(this.props.template)}</div>
+            <div className="row">
+                <div className="col-sm-3"></div>
+                <div 
+                    className="col-sm-6"
+                    style={{
+                        borderRadius: '5px',
+                        backgroundColor: '#F6F2F2',
+                        padding: '10px',
+                        marginTop: '30px'
+                    }}>
+                    <div className="row">
+                        <div className="col-md-6">
+                            <p className="text-left">
+                                <b>Id:</b>
+                            </p>
+                        </div>
+                        <div className="col-md-6">
+                            <p className="text-right">
+                                {this.props.template.id}
+                            </p>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-md-6">
+                            <p className="text-left">
+                                <b>Data source id:</b>
+                            </p>
+                        </div>
+                        <div className="col-md-6">
+                            <p className="text-right">
+                                {this.props.template.dataSourceId}
+                            </p>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-md-6">
+                            <p className="text-left">
+                                <b>Name:</b>
+                            </p>
+                        </div>
+                        <div className="col-md-6">
+                            <p className="text-right">
+                                {this.props.template.name}
+                            </p>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-md-6">
+                            <p className="text-left">
+                                <b>Creation date:</b>
+                            </p>
+                        </div>
+                        <div className="col-md-6">
+                            <p className="text-right">
+                                {this.props.template.created}
+                            </p>
+                        </div>
+                    </div>
+                    <br/>
+                    <div className="row">
+                        <div className="col-md-6">
+                            <p className="text-left">
+                                <b>Filter:</b>
+                            </p>
+                        </div>
+                        <div className="col-md-6">
+                            <p className="text-right">
+                                {filter.leftVal} {filter.comparator} {filter.rightVal}
+                            </p>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-md-6">
+                            <p className="text-left">
+                                <b>Key selector:</b>
+                            </p>
+                        </div>
+                        <div className="col-md-6">
+                            <p className="text-right">
+                                {this.props.template.keySelector.returnValue}
+                            </p>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-md-6">
+                            <p className="text-left">
+                                <b>Map function:</b>
+                            </p>
+                        </div>
+                        <div className="col-md-6">
+                            <p className="text-right">
+                                {this.props.template.mapFunction.returnValue}
+                            </p>
+                        </div>
+                    </div>
+                    <br/>
+                    <div className="row">
+                        <div className="col-sm-12">
+                            <button 
+                                type="button" 
+                                style={{ margin: '3px' }} 
+                                className="btn btn-danger float-right">
+                                Remove
+                            </button>
+                            <button 
+                                type="button" 
+                                style={{ margin: '3px' }} 
+                                className="btn btn-info float-right">
+                                Visualize
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         );
     }
 
